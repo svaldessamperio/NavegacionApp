68c225c3ff17a64ff6e52a36b76e05b6bbb434f0
svaldessamperio <svaldes_samperio@hotmail.com>
2021-06-17 22:59:53 -0500

    Ultimos Ajustes

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { styles } from '../theme/AppTheme';
 
 export default function SettingsScreen() {
 
-    const instop = useSafeAreaInsets();
+    const insets = useSafeAreaInsets();
     return (
-        <View style={{ marginTop:instop.top }}>
-            <Text>Settings Screen</Text>
+        <View style={{
+            ...styles.globalMarging,
+            marginTop:insets.top + 20,
+        }}
+        >
+            <Text style={{fontSize: 30, }}>Settings Screen</Text>
         </View>
     )
 }
