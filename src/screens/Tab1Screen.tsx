8303fff08b6e226b40e60ef2ecb3783ba124938d
Samuel Valdes Samperio <svaldes_samperio@hotmail.com>
2021-06-19 07:59:11 -0500

    usando el hook useSafeAreaInsets

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -2,14 +2,20 @@ import React, { useEffect }  from 'react'
 import { View, Text } from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons';
 import { styles } from '../theme/AppTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function Tab1Screen() {
+
+    const { top } = useSafeAreaInsets();
 
     useEffect(() => {
         console.log('Tab1Screen');
     }, [])
     return (
-        <View style={styles.globalMarging}>
+        <View style={{
+            ...styles.globalMarging,
+            marginTop: top + 10,
+        }}>
             <Text style={styles.title}>Iconos</Text>
             <Icon name="airplane-outline" size={50} color="#900" />
         </View>
